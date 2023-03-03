aba3cf4a7b6617b9f9ecff02ff307a170a5211cb
Alexander Fursa <gdm73078@gmail.com>
2023-03-03 12:51:08 +0100

    Add season enum

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,26 @@
+
+
+
+
+
+
+
+
+
+
 public class Main {
+
+
+
+  enum Season {
+WINTER,
+    SPRING,
+    SUMMER,
+    FALL,
+    AUTUMN,
+
+
+  }
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
